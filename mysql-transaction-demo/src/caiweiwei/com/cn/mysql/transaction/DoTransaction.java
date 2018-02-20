ceb028bd1e0ef7ce823408cc48a5f63275a9a9a5
hustdc <dcduanman@163.com>
2018-02-20 10:49:25 +0800

    transaction example

--- mysql-transaction-demo/src/caiweiwei/com/cn/mysql/transaction/DoTransaction.java
+++ mysql-transaction-demo/src/caiweiwei/com/cn/mysql/transaction/DoTransaction.java
@@ -20,23 +20,21 @@ public class DoTransaction {
             //注意与在数据库中操作有点不同，数据库为set autocommit = 0;  
             tj.con.setAutoCommit(false);  
             //利用占位符，减少字符串拼接的麻烦  
-            String sql1 = "insert into record (id,date,money,state) values (?,?,?,?);";  
+            String sql1 = "insert into user (id,password,money) values (?,?,?);";  
           
             //插入一条存入200块钱的数据  
             tj.ps = tj.con.prepareStatement(sql1);  
             tj.ps.setInt(1, 1003);  
-            tj.ps.setLong(2, new Date().getTime());  
+            tj.ps.setString(2, "dddd");  
             tj.ps.setInt(3, 100);  
-            tj.ps.setString(4, "存入");  
           
             //修改添加删除信息返回的是受影响行数  
             int row = tj.ps.executeUpdate();  
             //在同时插入一条取出300的数据记录  
             tj.ps = tj.con.prepareStatement(sql1);  
             tj.ps.setInt(1, 1003);  
-            tj.ps.setLong(2, new Date().getTime());  
-            tj.ps.setInt(3, 300);  
-            tj.ps.setString(4, "取出");  
+            tj.ps.setString(2, "dddd");  
+            //tj.ps.setInt(3, 300);   
             int row2 = tj.ps.executeUpdate();  
               
             //提交事务  
@@ -54,7 +52,7 @@ public class DoTransaction {
           
           
         //然后来查询下表  
-        String sql2 = "select * from record;";  
+        String sql2 = "select * from user;";  
           
           
           
@@ -69,16 +67,14 @@ public class DoTransaction {
           
         try {  
             while (tj.rs.next()) {  
-            //控制台输出下查询到的信息  
-            System.out.println(tj.rs.getInt("id")+"\t"+new Timestamp(tj.rs.getLong("date"))+"\t"+  
-            tj.rs.getInt("money")+"\t"+tj.rs.getString("state"));  
-                  
+                //控制台输出下查询到的信息  
+                System.out.println(tj.rs.getInt("id")+"\t"+tj.rs.getString("password")+"\t"+  
+                tj.rs.getInt("money"));  
             }  
         } catch (SQLException e) {  
               
             e.printStackTrace();  
-        }  
-          
+        }
           
         try {  
             tj.closeCon();//关闭数据连接  
