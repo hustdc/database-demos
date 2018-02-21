b8c15e750a50f5d482e7bb0b87a9e1886f0157c9
hustdc <dcduanman@163.com>
2018-02-21 13:23:14 +0800

    add 1to2 resultMap demo

---
+++ mybatis-demo/src/caiweiwei/com/cn/fkit/domain/Clazz.java
@@ -0,0 +1,22 @@
+package caiweiwei.com.cn.fkit.domain;
+
+public class Clazz {
+	private Integer id;
+	private String code;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public void setCode(String code) {
+		this.code = code;
+	}
+}
--- mybatis-demo/src/caiweiwei/com/cn/fkit/main/MyBatisTest.java
+++ mybatis-demo/src/caiweiwei/com/cn/fkit/main/MyBatisTest.java
@@ -2,6 +2,7 @@ package caiweiwei.com.cn.fkit.main;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -9,6 +10,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import caiweiwei.com.cn.fkit.domain.User;
+import caiweiwei.com.cn.fkit.domain.Student;
 
 public class MyBatisTest {
 
@@ -19,6 +21,14 @@ public class MyBatisTest {
 		SqlSession session = sqlSessionFactory.openSession();
 		User user = new User("admin", "男", 26);
 		session.insert("caiweiwei.com.cn.fkit.mapper.UserMapper.save", user);
+		session.commit();
+		
+		List<Student> studentList = session.selectList("caiweiwei.com.cn.fkit.mapper.UserMapper.selectStudent");
+		for (Student student : studentList) {
+			String studentName = student.getName();
+			String studentClazzCode = student.getClazz().getCode();
+			System.out.println(studentName + " : " + studentClazzCode);
+		}
 		session.commit();
 		session.close();
 	}
